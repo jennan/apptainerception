dbc5c36891507dbfbb6b159048d15972f8579065
Maxime RIO <maxime.rio@niwa.co.nz>
2022-09-24 10:46:58 +0000

    make build script more robust, force cache dir and build dir

--- builder.bash
+++ builder.bash
@@ -12,6 +12,16 @@ module load Apptainer/1.0.3
 module unload XALT
 
 # ensure cache and buildir are defined and pased to the builder container
+if [ -z "${APPTAINER_CACHEDIR:-}" ]; then
+    echo "Set APPTAINER_CACHEDIR as your cache directory before running this script."
+    exit 1
+fi
+
+if [ -z "${APPTAINER_TMPDIR:-}" ]; then
+    echo "Set APPTAINER_TMPDIR as your build directory before running this script."
+    exit 1
+fi
+
 export APPTAINERENV_APPTAINER_CACHEDIR="$APPTAINER_CACHEDIR"
 export APPTAINERENV_APPTAINER_TMPDIR="$APPTAINER_TMPDIR"
 export APPTAINER_BINDPATH="$APPTAINER_BINDPATH,$APPTAINER_TMPDIR,$APPTAINER_CACHEDIR"
